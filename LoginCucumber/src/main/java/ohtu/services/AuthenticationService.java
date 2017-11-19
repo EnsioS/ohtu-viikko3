c4dc30ff4624bd9669c354a447fb1f373fb431f8
EnsioS <ensiosuonpera@luukku.com>
2017-11-19 23:37:38 +0200

    vk3 teht7

--- LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -40,7 +40,22 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
-
+        if (username.length() < 3 || password.length() < 3) {
+            return true;
+        }
+        
+        int laskuri = 0;
+        
+        for (int i = 0; i < password.length(); i++) {    
+            if (Character.isAlphabetic(password.codePointAt(i))) {
+                laskuri++;
+            }
+        }
+        
+        if (laskuri == password.length()) {
+            return true;
+        }
+        
         return false;
     }
 }
